a0ea5ddd4c905c5b361c7ca2196707cd417e5e8e
Kevin Ruscoe <kevdotbadger@gmail.com>
2023-01-10 16:02:03 +0000

    add fonts so we dont rely on google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,40 @@
 module.exports = {
   content: [
-    './public/**/*.{html,php}',
-    './src/js/**/*.js'
+    './public/**/*.{php,html}',
   ],
   theme: {
-    extend: {},
+    container: {
+      padding: '2rem',
+      center: true
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1440px',
+    },
+    extend: {
+      fontFamily: {
+        "header": ['"Sansation"', 'sans-serif'],
+        "body": ['"Hind Siliguri"', 'sans-serif'],
+        "libre": ['"Libre Baskerville"']
+      },
+      colors: {
+        theme: {
+          teal: "#73c9c4",
+          gray: {
+            DEFAULT: "#808182",
+            light: '#e7e7e7',
+            dark: '#222222'
+          }
+        }
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }

f7e2b2a82194ba434a00814f49cf29e281b9603e
Kevin Ruscoe <kevdotbadger@gmail.com>
2023-02-01 12:24:06 +0000

    revert

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,8 @@
 import Alpine from 'alpinejs'
-import collapse from '@alpinejs/collapse'
 import welcome from './components/welcome'
  
 window.Alpine = Alpine
 
-// Alpine.data('welcome', welcome)
+Alpine.data('welcome', welcome)
  
 Alpine.start()
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,33 +3,6 @@ module.exports = {
     './public/**/*.{php,html}',
   ],
   theme: {
-    container: {
-      padding: '2rem',
-      center: true
-    },
-    screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1440px',
-    },
-    extend: {
-      fontFamily: {
-        "header": ['"Sansation"', 'sans-serif'],
-        "body": ['"Hind Siliguri"', 'sans-serif'],
-        "libre": ['"Libre Baskerville"']
-      },
-      colors: {
-        theme: {
-          teal: "#73c9c4",
-          gray: {
-            DEFAULT: "#808182",
-            light: '#e7e7e7',
-            dark: '#222222'
-          }
-        }
-      },
-    },
   },
   variants: {
     extend: {},

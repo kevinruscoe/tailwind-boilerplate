7699751f77e10a067f189d42ce96175b154ea37f
Kevin Ruscoe <kevdotbadger@gmail.com>
2023-02-01 12:18:10 +0000

    remove cdn links

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
 import Alpine from 'alpinejs'
+import collapse from '@alpinejs/collapse'
 import welcome from './components/welcome'
  
 window.Alpine = Alpine
 
-Alpine.data('welcome', welcome)
+// Alpine.data('welcome', welcome)
  
 Alpine.start()
